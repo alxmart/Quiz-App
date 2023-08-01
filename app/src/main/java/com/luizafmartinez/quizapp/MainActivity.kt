4759301eb8b1db24e940592ba4a63ef32d58cffb
alxmart <alxmartinez@gmail.com>
2023-08-01 19:00:32 -0300

    Code for field validation (name)

--- app/src/main/java/com/luizafmartinez/quizapp/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/quizapp/MainActivity.kt
@@ -2,10 +2,52 @@ package com.luizafmartinez.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputLayoutNome: TextInputLayout
+    private lateinit var editNome: TextInputEditText
+    private lateinit var btnIniciar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inicializarComponentesInterface()
+        inicializarEventosClique()
+    }
+
+    private fun inicializarEventosClique() {
+        btnIniciar.setOnClickListener { 
+          validarCampos()
+        }
+    }
+    private fun validarCampos() {
+
+        val nome = editNome.text.toString()
+        if ( nome.isNotEmpty() ) {
+            textInputLayoutNome.error = null
+        } else {
+            textInputLayoutNome.error = "Preencha seu nome para prosseguir"
+        }
+    }
+
+    private fun inicializarComponentesInterface() {
+        textInputLayoutNome = findViewById(R.id.text_input_layout_nome)
+        editNome            = findViewById(R.id.edit_nome)
+        btnIniciar          = findViewById(R.id.btn_iniciar)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
