f415606e679ddb4e8f0ac7dc0659c7f9af4e1a4d
alxmart <alxmartinez@gmail.com>
2023-08-09 14:15:52 -0300

    Adding id and components

--- app/src/main/java/com/luizafmartinez/quizapp/PerguntasActivity.kt
+++ app/src/main/java/com/luizafmartinez/quizapp/PerguntasActivity.kt
@@ -2,17 +2,51 @@ package com.luizafmartinez.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputLayout
 
 class PerguntasActivity : AppCompatActivity() {
 
-    private lateinit var textExibicaoNome: TextView
+    // Componentes
+    private lateinit var textExibicaoNome : TextView
+    private lateinit var textExibicaoREsumo : TextView
+    private lateinit var textTitulo : TextView
+    private lateinit var radioResposta1 : RadioButton
+    private lateinit var radioResposta2 : RadioButton
+    private lateinit var radioResposta3 : RadioButton
+    private lateinit var btnConfirmar : Button
+
+
+
+    private lateinit var listaPerguntas : List<Pergunta>
+    private lateinit var perguntaAtual : Pergunta
+    private var indicePerguntaAtual = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perguntas)
+
         inicializarComponentesInterface()
+
+        // Carregar dados
+        // Pergunta1, pergunta2, pergunta3
+        listaPerguntas = DadosPerguntas.retornarListaPerguntas()
+
+        exibirDadosPerguntaAtual()
+
+    }
+
+    private fun exibirDadosPerguntaAtual() {
+
+        perguntaAtual = listaPerguntas[indicePerguntaAtual]
+        // Exibir os dados
+
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
     private fun inicializarComponentesInterface() {
 
